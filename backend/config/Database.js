9b601037bb7660383634028950b582c13fc44697
royanaditya <151741230+royanaditya@users.noreply.github.com>
2025-03-24 00:42:52 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 // Nyambungin db ke BE
-const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+const db = new Sequelize("hrd", "root", "", {
   host: "35.226.28.38", // IP publik dari GCP instance
   dialect: "mysql",
 });
